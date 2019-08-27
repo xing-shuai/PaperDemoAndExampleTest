c44ee8d4caa270c06eb9aa0f43a3a86707dae2e7
xing-shuai <shuaihongjie@outlook.com>
2019-08-27 20:49:04 +0800

    Svd solves non-singular equations

---
+++ SVD_non_singular_equation.py
@@ -0,0 +1,18 @@
+import numpy as np
+
+a = np.array([[1.1, -0.1],
+              [-0.1, 1.1]])
+
+b = np.array([[86.6], [103.4]])
+
+u, d, v = np.linalg.svd(a)
+
+print(u, d, v)
+
+b_ = u.T @ b
+
+y = [b_[i, 0] / val for i, val in enumerate(d)]
+
+x = v.T @ np.expand_dims(np.array(y), 1)
+
+print(x)
